9aafc2104d0aea310e0acc2f1b9d0a1aca9158e8
mrenninger <me@michaelrenninger.com>
2014-12-29 10:38:54 -0800

    initial commit

---
+++ RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  RedBalloons
+//
+//  Created by Michael Renninger on 11/24/14.
+//  Copyright (c) 2014 Michael Renninger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon{
+    var img = UIImage(named:"")
+    var num:Int = 0
+}
--- RedBalloons/ViewController.swift
+++ RedBalloons/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
+    
+    var images = ["RedBalloon1.jpg","RedBalloon2.jpg","RedBalloon3.jpg","RedBalloon4.jpg"]
+    var curBalloonNum = 0;
+    var curRandomNum = 0;
 
+    var balloons:[Balloon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i = 0; i < 99; i++ {
+            var tBalloon = Balloon()
+            tBalloon.num = i + 1
+            
+            var randNum:Int
+            do {
+                randNum = Int(arc4random_uniform(UInt32(images.count)))
+            } while curRandomNum == randNum
+            
+            curRandomNum = randNum
+            tBalloon.img = UIImage(named:images[randNum])
+            
+            
+            balloons.append(tBalloon)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onNextBtnPressed(sender: UIBarButtonItem) {
+        self.img.image = balloons[curBalloonNum].img
+        self.lbl.text = "Balloon \(balloons[curBalloonNum].num)"
+        
+        curBalloonNum++
+        
+        if curBalloonNum == 99 {
+            curBalloonNum = 0
+        }
+    }
 
 }
 
